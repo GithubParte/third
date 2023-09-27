e7504aa2f36fe5d3395f94fdf634fd4104c3819a
Parisa Tejari <parisa.tejari@uib.no>
2023-09-28 01:12:49 +0200

    tests

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestClass {
+    @Test
+    public void fakeTest(){
+        int sum= 3+4;
+        Assertions.assertEquals(sum, 7);
+    }
+}
